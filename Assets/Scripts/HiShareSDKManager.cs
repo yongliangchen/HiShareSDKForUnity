dfcbe030014cad56caf5352fb75d4683ee79a242
陈永亮 <451319296@qq.com>
2020-10-10 19:02:26 +0800

    update：添加了QQ框架

---
+++ Assets/Scripts/HiShareSDKManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HiShareSDK
+{
+    public class HiShareSDKManager : MonoSingleton<HiShareSDKManager>
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlatformType.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HiShareSDK
+{
+    public enum PlatformType
+    {
+        /// <summary>QQ好友</summary>
+        QQ,
+        /// <summary>QQ空间</summary>
+        QZone,
+        /// <summary>微信</summary>
+        WeChat,
+        /// <summary>微信朋友圈</summary>
+        WeChatMoments,
+        /// <summary>新浪微博</summary>
+        SinaWeibo,
+    }
+}
